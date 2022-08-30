81372592db711ef369b6a9b330d5e42ac94267eb
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-08-30 15:19:45 +0500

    complete

--- public/main.js
+++ public/main.js
@@ -1,7 +1,44 @@
+
+const socket = io.connect("http://localhost:8000/")
+const inputEl = document.querySelector("#message")
+
+let color;
+socket.on("new-message",(data)=>console.log(data))
+socket.on("clearBoard",()=>setup())
+function sendMessage(e){
+  socket.emit('chat-message',inputEl.value)
+  inputEl.value =''
+}
+function clearDrawing(e){
+  socket.emit('clearBtn')
+}
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+  background(70);
   }
-  
-  function draw() {
-    background(220);
+  // function draw() {
+    
+    // }
+    socket.on("broadcastDrawing", (history)=>{
+      noStroke()
+      history.forEach(ele => {
+        fill(ele.c[0],ele.c[1],ele.c[2])
+        ellipse(ele.x, ele.y, 10, 10)
+      });
+    })
+    socket.on("initializeBroadcast", (data)=>{
+      noStroke()
+      color =data[1]
+      data[0].forEach(ele => {
+        fill(ele.c[0],ele.c[1],ele.c[2])
+        ellipse(ele.x, ele.y, 10, 10)
+      });
+    })
+    function mouseDragged(){
+    noStroke()
+    fill(color[0],color[1],color[2])
+    ellipse(mouseX, mouseY, 10, 10);
+    socket.emit('drawnByClient',{x:mouseX,y:mouseY,c:color})
   }
+
+  // draw()
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const { Server } = require("socket.io");
 
 const app = express();
 
 app.use(express.static("public"));
 
 
-app.listen(8000);
+const httpServer = app.listen(8000);
+const io = new Server(httpServer)
+let historyArr = []
+io.on("connection",(socket) => {
+  console.log("Client Connected", socket.id)
+  socket.on("chat-message",(data)=>{
+    console.log(data +" by " +socket.id)
+    socket.broadcast.emit("new-message",`${data} by ${socket.id}`)
+  })
+  socket.on("drawnByClient",(data)=>{
+     historyArr.push(data)
+    socket.broadcast.emit("broadcastDrawing",historyArr)
+  })
+  socket.emit("initializeBroadcast",[historyArr,[Math.ceil(Math.random()*265),Math.ceil(Math.random()*265),Math.ceil(Math.random()*265)]])
+  socket.on("clearBtn",()=>{
+    historyArr = []
+    io.emit("clearBoard")
+  })
+})
